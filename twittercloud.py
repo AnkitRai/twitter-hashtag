e845fc32ef590d73dfe2c7a9353e8338bf49319b
Ankit Rai <ankit.rai09@gmail.com>
2015-06-25 00:54:23 -0500

    changing the code for writing tweets to text file as well generate infograph

--- twittercloud.py
+++ twittercloud.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-# Contact at rai5@illinois.edu for any queries
-# Adapted by Mining the Social Web by Mathew A. Russell
+
+# For any queries please contact at rai5@illinois.edu
+# Adapted from Mining the Social Web 2nd Edition by Mathew A. Russell
 
 from __future__ import print_function
 import re
@@ -12,13 +13,15 @@ from pytagcloud import create_tag_image, make_tags
 
 def search_twitter(twitter_api, q, search_size = 100, stop_count = 1000):
     '''
-    
-    Parameters:
-      twitter_api: Use twitter.Twitter to create twitter.api.Twitter object.
-      q (str): search query (e.g. #green infrastructure)
-      search_size: default 100.
-      stop_count: stops search when the total size of tweets exceeds stop_count.
-    '''
+Modified from Example 1-5 in Mining the Social Web 2nd Edition.
+Returns statuses, a list of dictionaries of twitter metadata.
+
+Parameters:
+twitter_api: Use twitter.Twitter to create twitter.api.Twitter object.
+q (str): search query (e.g. #informatics)
+search_size: default 100.
+stop_count: stops search when the total size of tweets exceeds stop_count.
+'''
     # See https://dev.twitter.com/docs/api/1.1/get/search/tweets
     search_results = twitter_api.search.tweets(q = q, count = search_size)
     statuses = search_results['statuses']
@@ -42,56 +45,64 @@ def search_twitter(twitter_api, q, search_size = 100, stop_count = 1000):
 
 def clean_statuses(statuses):
     '''
-    Takes a list of dictionaries of tweet metadata returned from
-    search_twitter() function, and returns a list with all lowercase words
-    (no words with #, @, http, or non-alphabetical characters).
+Takes a list of dictionaries of tweet metadata returned from
+search_twitter() function, and returns a list with all lowercase words
+(no words with #, @, http, or non-alphabetical characters).
 
-    Parameters:
-      statuses: a list of dictionaries of tweet metadata returned from
-                search_twitter() function.
-    '''
+Parameters:
+statuses: a list of dictionaries of tweet metadata returned from
+search_twitter() function.
+'''
     status_texts = [status['text'] for status in statuses]
     status_texts = [text.encode('ascii', 'ignore') for text in status_texts]
 
     clean_tweets = []
-    clean_tweets = [w for t in status_texts
-                        for w in t.split()]
 
     
+    string_list = []    
     
+    # seperate every string into words and add to the list string_list
+    for item in status_texts: 
+        string_list += str(item).split()
+    
+    # checks for non-alphabetical (covers @ and #) and also 'http'
+    r = re.compile('[^a-zA-Z]|http')
+    
+    # replace string with empty string if necessary and append to clean_tweets
+    for item in string_list: # go through each string in the list 
+        if re.match(r, str(item)): # if matches, replace with empty string here
+            item = ''
+        clean_tweets.append(item) # append the string to the list
+    
+    clean_tweets = filter(None,clean_tweets) # delete empty strings
+    clean_tweets = [x.lower() for x in clean_tweets] # convert all strings to lowercase
+
     return clean_tweets
 
 def get_counts(words):
     '''
-    Takes a list of strings and returns a dictionary of {string: frequency}.
+Takes a list of strings and returns a dictionary of {string: frequency}.
 
-    Parameters:
-      words: a list of strings
+Parameters:
+words: a list of strings
 
-    Examples:
-    >>> get_counts(['a', 'a', 'b', 'b', 'b', 'c'])
-    [('b', 3), ('a', 2), ('c', 1)]
-    '''
+Examples:
+>>> get_counts(['a', 'a', 'b', 'b', 'b', 'c'])
+[('b', 3), ('a', 2), ('c', 1)]
+'''
+    series = pd.Series(words) # create pandas series object
+    counts = series.value_counts() # get the frequency count for each tag
+    counts = [item for item in counts.iteritems()] # convert to a tuple of form [('a',3),('b',2)]
 
-    
-    counts = dict()
-    for item in counts:
-        if item not in counts:
-            counts[item] = 1
-        else:
-            counts[item] += 1
-    
     return counts
 
 
 def main():
     
-    # https://dev.twitter.com/docs/auth/oauth for more information 
-    # on Twitter's OAuth implementation.
-    CONSUMER_KEY = '7J6ZlYXjP1xOwimtaiADtvUD1'
-    CONSUMER_SECRET = 'wPLHMfPqss9UXMHWd5wnbMk6lXWS5FSHxwGeOc7nzHDbqYzxBg'
-    OAUTH_TOKEN = '410357936-hSdUGLvx8Lv3PNebvxx6gyeJX6q9n2C9vCnQ9Go4'
-    OAUTH_TOKEN_SECRET = 'hWbOZQTCtheoiAR7c5JEBZ9hKb7tZfq4cGUKbgUs7ZmQo'
+    CONSUMER_KEY = 'JckMGEMIejRMC7hoE3d6oRpmj'
+    CONSUMER_SECRET = 'Vh1GSMGetuAvG7EswY0k2rCNls1U0v6hTFAl40dSL4TRQ6mREy'
+    OAUTH_TOKEN = '410357936-w2p40cNA8FpjyFyuJDpmy3js87jo6lPcegsKUUa6'
+    OAUTH_TOKEN_SECRET = 'ceUusyIEvH5EVYFqUIUVXjoiKAY5Dr3dZbOshRN5RSl91'
 
     auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                                CONSUMER_KEY, CONSUMER_SECRET)
@@ -99,7 +110,7 @@ def main():
     twitter_api = twitter.Twitter(auth = auth)
 
     # Search query, try your own.
-    q = '#green infrastructure' # try different hash tag examples
+    q = '#green infrastructure'
 
     # calling search_twitter too often will lock you out for 1 hour.
     # we will call search twitter once and save the result in a file.
@@ -111,11 +122,15 @@ def main():
     results = pickle.load(open('{0}.p'.format(q), 'rb'))
     # clean the tweets and extract the words we want
     clean_tweets = clean_statuses(results)
+    f = open('tweet.txt','w')
+    for item in pd.Series(clean_tweets):
+        f.write(item)
+    f.close()
     # calculate the frequency of each word
     word_count = get_counts(clean_tweets)
 
     # use PyTagCloud to create a tag cloud
-    tags  = make_tags(word_count, maxsize = 120)
+    tags = make_tags(word_count, maxsize = 120)
     # the image is store in 'cloud.png'
     create_tag_image(tags, 'cloud.png', size = (900, 600), fontname = 'Lobster')
     
